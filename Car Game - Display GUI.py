e2e39a17f71ae903c53b263207bd5ceaf69ceb66
Ethan22112 <22112@fraser.school.nz>
2026-05-14 10:42:16 +1200

    Removing a Few Variables And Functions
    Removing Player Variables And Functions to draw it on screen, as this file is only meant to test the basic GUI.

---
+++ Car Game - Display GUI.py
@@ -0,0 +1,28 @@
+import pygame
+import time
+
+#Initializtion Of Pygame
+pygame.init()
+
+Quit_Game = False
+
+#Initialize Screen Width And Height
+FRAME_WIDTH = 1000
+FRAME_HEIGHT = 500
+
+RED = (255, 0, 0)
+
+#Initialize Window And Title
+WINDOW = pygame.display.set_mode((FRAME_WIDTH, FRAME_HEIGHT))
+pygame.display.set_caption("Street Racer")
+
+
+#Main Game Loop
+while not Quit_Game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            Quit_Game = True
+
+
+pygame.quit()
+quit()
