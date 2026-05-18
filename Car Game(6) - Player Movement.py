f0825ee4bf54b289edf43270debd78d24d3ef9be
Ethan22112 <22112@fraser.school.nz>
2026-05-18 13:15:57 +1200

    Create Car Game(6) - Player Movement
    creating another iteration of the project that implements player movement.

---
+++ Car Game(6) - Player Movement.py
@@ -0,0 +1,108 @@
+import pygame
+import random
+
+#Initializtion Of Pygame
+pygame.init()
+
+Quit_Game = False
+
+#Initialize Screen Width And Height
+FRAME_WIDTH = 1000
+FRAME_HEIGHT = 500
+
+
+#Initialize Colors
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+
+#Initialize Window And Title
+WINDOW = pygame.display.set_mode((FRAME_WIDTH, FRAME_HEIGHT))
+pygame.display.set_caption("Street Racer")
+
+
+#Initialize The Player's car variables
+PLAYER_WIDTH = 20
+PLAYER_HEIGHT = 10
+
+Player_X = FRAME_WIDTH / 2
+Player_Y = FRAME_HEIGHT / 2
+
+PLAYER_COLOR = RED
+
+
+#Initialize The Obstacle Car's Variables
+OBSTACLE_CAR_WIDTH = 20
+OBSTACLE_CAR_HEIGHT = 10
+
+Obstacle_car_X = FRAME_WIDTH - OBSTACLE_CAR_WIDTH
+Obstacle_car_Y = random.randint(0, FRAME_HEIGHT)
+
+OBSTACLE_CAR_COLOR = BLUE
+Obstacle_car_speed = random.randint(1, 5)
+
+TOTAL_NUM_OBSTACLE_CARS = 10
+obstacle_cars = []
+
+
+#Setting The Game's Clock & FPS
+clock = pygame.time.Clock()
+FPS = 60
+
+
+#Obstacle Car Class - creates one instance of an obstacle car if called
+class Obstacle_Car:
+    def __init__(self, color, speed, x, y, width, height):
+
+        #sets the car instance's variables
+        self.Obstacle_car_speed = speed
+        self.Obstacle_car_WIDTH = width
+        self.Obstacle_car_HEIGHT = height
+        self.Obstacle_car_X = x
+        self.Obstacle_car_Y = y
+        self.Obstacle_car_COLOR = color
+
+    #function that controls the movement of the car where if called, the car will move 
+    def Go(self):
+       self.Obstacle_car_X -= self.Obstacle_car_speed
+
+       if (self.Obstacle_car_X + self.Obstacle_car_WIDTH) < 0:
+            self.Obstacle_car_X = FRAME_WIDTH
+            self.Obstacle_car_Y = random.randint(0, FRAME_HEIGHT)
+            self.Obstacle_car_speed = random.randint(1, 5)
+     
+       pygame.draw.rect(WINDOW, self.Obstacle_car_COLOR, (self.Obstacle_car_X, self.Obstacle_car_Y, self.Obstacle_car_WIDTH, self.Obstacle_car_HEIGHT))
+
+
+#Create Multiple Instances Of The Obstacle Car 
+for i in range(0, TOTAL_NUM_OBSTACLE_CARS):
+    obstacle_car = Obstacle_Car(BLUE, random.randrange(1, 5), random.randrange(FRAME_WIDTH, FRAME_WIDTH * 2), random.randrange(0, FRAME_HEIGHT), OBSTACLE_CAR_WIDTH, OBSTACLE_CAR_HEIGHT)
+    obstacle_cars.append(obstacle_car)
+
+
+#Main Game Loop
+while not Quit_Game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            Quit_Game = True
+
+    #Setting The Screen To Black Before Drawing Again
+    WINDOW.fill(BLACK)
+ 
+    #draw a filled rectangle in the screen with the player's specifications
+    pygame.draw.rect(WINDOW, PLAYER_COLOR, (Player_X, Player_Y, PLAYER_WIDTH, PLAYER_HEIGHT))
+
+    
+    #Call all the instances of the obstacle cars to move
+    for i in range(0, TOTAL_NUM_OBSTACLE_CARS):
+        obstacle_cars[i].Go()
+   
+    #refreshes the screen at a set frame rate
+    clock.tick(FPS)
+
+    #refreshes the screen 
+    pygame.display.flip()
+
+
+pygame.quit()
+quit()
