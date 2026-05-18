5621e999f1aaea77700c2e75b3e8978c45af8974
Ethan22112 <22112@fraser.school.nz>
2026-05-18 12:55:08 +1200

    Create Car Game Ob car instances
    creating a class within the project that handles one or multiple instances of an obstacle car.

---
+++ Car Game - Ob_car_instances.py
@@ -0,0 +1,98 @@
+import pygame
+import random
+
+#Initializtion Of Pygame
+pygame.init()
+
+Quit_Game = False
+
+#Initialize Screen Width And Height
+FRAME_WIDTH = 1000
+FRAME_HEIGHT = 500
+
+
+#Initialize Colors
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+
+#Initialize Window And Title
+WINDOW = pygame.display.set_mode((FRAME_WIDTH, FRAME_HEIGHT))
+pygame.display.set_caption("Street Racer")
+
+
+#Initialize The Player's car variables
+PLAYER_WIDTH = 20
+PLAYER_HEIGHT = 10
+
+Player_X = FRAME_WIDTH / 2
+Player_Y = FRAME_HEIGHT / 2
+
+PLAYER_COLOR = RED
+
+
+#Initialize The Obstacle Car's Variables
+OBSTACLE_CAR_WIDTH = 20
+OBSTACLE_CAR_HEIGHT = 10
+
+Obstacle_car_X = FRAME_WIDTH - OBSTACLE_CAR_WIDTH
+Obstacle_car_Y = random.randint(0, FRAME_HEIGHT)
+
+OBSTACLE_CAR_COLOR = BLUE
+Obstacle_car_speed = random.randint(1, 5)
+
+#Setting The Game's Clock & FPS
+clock = pygame.time.Clock()
+FPS = 60
+
+
+#Obstacle Car Class - creates one instance of an obstacle car if called
+class Obstacle_Car:
+    def __init__(self, color, speed, x, y, width, height):
+
+        self.Obstacle_car_speed = speed
+        self.Obstacle_car_WIDTH = width
+        self.Obstacle_car_HEIGHT = height
+        self.Obstacle_car_X = x
+        self.Obstacle_car_Y = y
+        self.Obstacle_car_COLOR = color
+
+    def Go(self):
+       self.Obstacle_car_X -= self.Obstacle_car_speed
+
+       if (self.Obstacle_car_X + self.Obstacle_car_WIDTH) < 0:
+            self.Obstacle_car_X = FRAME_WIDTH
+            self.Obstacle_car_Y = random.randint(0, FRAME_HEIGHT)
+            self.Obstacle_car_speed = random.randint(1, 5)
+     
+       pygame.draw.rect(WINDOW, self.Obstacle_car_COLOR, (self.Obstacle_car_X, self.Obstacle_car_Y, self.Obstacle_car_WIDTH, self.Obstacle_car_HEIGHT))
+
+#Create one instance of an obstacle car for testing purposes
+Obstacle_Car1 = Obstacle_Car(BLUE, Obstacle_car_speed, Obstacle_car_X, Obstacle_car_Y, OBSTACLE_CAR_WIDTH, OBSTACLE_CAR_HEIGHT)
+
+
+#Main Game Loop
+while not Quit_Game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            Quit_Game = True
+
+    #Setting The Screen To Black Before Drawing Again
+    WINDOW.fill(BLACK)
+ 
+    #draw a filled rectangle in the screen with the player's specifications
+    pygame.draw.rect(WINDOW, PLAYER_COLOR, (Player_X, Player_Y, PLAYER_WIDTH, PLAYER_HEIGHT))
+
+    
+    #Call the one instance of the obstacle car
+    Obstacle_Car1.Go()
+   
+    #refreshes the screen at a set frame rate
+    clock.tick(FPS)
+
+    #refreshes the screen 
+    pygame.display.flip()
+
+
+pygame.quit()
+quit()
