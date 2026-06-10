e60fad08571de644b8db332da094997ac00b1083
Ethan22112 <22112@fraser.school.nz>
2026-06-11 09:51:28 +1200

    Integrating Graphics
    creating and implementing 'sprites' for the cars and the background, though the background does not move yet to create that illusion that the cars are moving.

--- Car Game(12) - Integrating Graphics.py
+++ Car Game(12) - Integrating Graphics.py
@@ -22,6 +22,10 @@ WINDOW = pygame.display.set_mode((FRAME_WIDTH, FRAME_HEIGHT))
 pygame.display.set_caption("Street Racer")
 
 
+#offset for display
+xOffset1 = 0
+xOffset2 = FRAME_WIDTH
+
 #Initialize The Player's car variables
 PLAYER_WIDTH = 20
 PLAYER_HEIGHT = 10
@@ -56,6 +60,12 @@ TOTAL_NUM_OBSTACLE_CARS = 10
 obstacle_cars = []
 
 
+#Initialize Background Variables
+ROAD_TILE = pygame.image.load("Sprites & Tiles/Road.png").convert_alpha()
+ROAD_WIDTH = ROAD_TILE.get_width()
+ROAD_HEIGHT = ROAD_TILE.get_height()
+
+
 #Setting The Game's Clock & FPS
 clock = pygame.time.Clock()
 FPS = 60
@@ -78,19 +88,19 @@ class Obstacle_Car:
 
     #function that controls the movement of the car where if called, the car will move 
     def Go(self):
-       self.Obstacle_car_X -= self.Obstacle_car_speed
+        self.Obstacle_car_X -= self.Obstacle_car_speed
 
-       if (self.Obstacle_car_X + self.Obstacle_car_WIDTH) < 0:
+        if (self.Obstacle_car_X + self.Obstacle_car_WIDTH) < 0:
             self.Obstacle_car_X = FRAME_WIDTH
             self.Obstacle_car_Y = random.randint(0, FRAME_HEIGHT)
             self.Obstacle_car_speed = random.randint(1, 5)
             self.isAlive = True
         
         # checks if the current car is still "alive", will continue to do as intended if so, will get "disabled" if not 
-       if self.isAlive == True:
+        if self.isAlive == True:
             WINDOW.blit(OBSTACLE_CAR_SPRITE, (self.Obstacle_car_X, self.Obstacle_car_Y))
             self.rect = pygame.Rect(self.Obstacle_car_X, self.Obstacle_car_Y, self.Obstacle_car_WIDTH, self.Obstacle_car_HEIGHT)
-       else:
+        else:
             self.rect = pygame.Rect(0, 0, 0, 0)
 
     #function that checks if this car collides with another car(not the player)
@@ -116,6 +126,13 @@ while not Quit_Game:
     #Setting The Screen To Black Before Drawing The Cars Or Background Again
     WINDOW.fill(BLACK)
 
+    #test background
+    for x in range(0, round(FRAME_WIDTH / ROAD_WIDTH)):
+        for y in range(0, round(FRAME_HEIGHT / ROAD_HEIGHT)):
+
+            WINDOW.blit(ROAD_TILE, (x * ROAD_WIDTH + xOffset1, y * ROAD_HEIGHT))
+             
+    
     #The code below handles user inputs especially the arrow keys, this controls player movement
 
     keys = pygame.key.get_pressed()
